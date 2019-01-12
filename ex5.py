1be65c2bf922d87b008779df0d71cd75ad8560a4
Johnny Martinez <johnny@Johnnys-MacBook-Pro-2.local>
2019-01-12 15:05:04 -0700

    Commiting ex 5

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Johnny Martinez'
+my_age = 36
+my_height = 74
+my_weight = 195
+my_eyes = 'brown'
+my_teeth = 'White'
+my_hair = 'brown'
+
+print(f"Let's Talsk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("actually, thats not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right.
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
