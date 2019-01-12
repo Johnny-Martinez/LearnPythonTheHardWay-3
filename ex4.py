7d27783268bdaeeefc3327f918af2f47855db2ce
Johnny Martinez <johnnymartinez@Johnnys-MacBook-Pro.local>
2019-01-12 13:29:31 -0700

    initial commit with exercizes 3 and 4

---
+++ ex4.py
@@ -0,0 +1,22 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+
+print("I will count my chickens:")
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why its false.")
+
+print("How about some more???")
+
+print("Is it greater???", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
