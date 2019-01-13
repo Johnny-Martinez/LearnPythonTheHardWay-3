1875ac62bb519b996f2aff45a563aed8a3e7279e
Johnny Martinez <johnny@Johnnys-MacBook-Pro-2.local>
2019-01-13 12:41:57 -0700

    commit ex10

---
+++ ex10.py
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabben in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+Ill do a list:
+\t* Cat Food
+\t* Fishies
+\t* catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
