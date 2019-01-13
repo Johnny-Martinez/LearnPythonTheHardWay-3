648d9e4a18f426f06b615bc0b18707270b64481f
Johnny Martinez <johnny@Johnnys-MacBook-Pro-2.local>
2019-01-12 18:44:45 -0700

    commiting ex7

--- ex5.py
+++ ex5.py
@@ -1,19 +1,19 @@
-my_name = 'Johnny Martinez'
-my_age = 36
-my_height = 74
-my_weight = 195
-my_eyes = 'brown'
-my_teeth = 'White'
-my_hair = 'brown'
+name = 'Johnny Martinez'
+age = 36
+height = 74
+weight = 195
+eyes = 'brown'
+teeth = 'White'
+hair = 'brown'
 
-print(f"Let's Talsk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"Let's Talsk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
 print("actually, thats not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # This line is tricky, try to get it exactly right.
 
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
